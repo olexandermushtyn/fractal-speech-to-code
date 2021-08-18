48b9c812d4c6683d7fdeab65a97c00ac6774fe6d
jazzkrop <jazzkrop@gmail.com>
2021-08-18 19:19:04 +0300

    add is available. deleting by id availble. auto generatin unique id is available

--- bin/www.js
+++ bin/www.js
@@ -1,12 +1,12 @@
-const app = require("../app");
-const http = require("http");
-const normalizePort = require("./normalizePort");
+const app = require('../app')
+const http = require('http')
+const normalizePort = require('./normalizePort')
 
 // Get port from environment and store in Express.
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+const port = normalizePort(process.env.PORT || '3000')
+app.set('port', port)
 
 // Create HTTP server.
-const server = http.createServer(app);
+const server = http.createServer(app)
 
-module.exports = server;
+module.exports = server
--- src/api/services/buildCommandsDataset.js
+++ src/api/services/buildCommandsDataset.js
@@ -1,23 +1,23 @@
-const fs = require("fs");
+const fs = require('fs')
 
 // Reading atoms (actions, elements). Smallest building blocks
-const actionsContent = fs.readFileSync("./src/api/entities/actions.json");
-const elementsContent = fs.readFileSync("./src/api/entities/elements.json");
+const actionsContent = fs.readFileSync('./src/api/entities/actions.json')
+const elementsContent = fs.readFileSync('./src/api/entities/elements.json')
 
-const actions = Object.keys(JSON.parse(actionsContent));
-const elements = Object.keys(JSON.parse(elementsContent));
+const actions = Object.keys(JSON.parse(actionsContent))
+const elements = Object.keys(JSON.parse(elementsContent))
 
-let commands = [];
+let commands = []
 
 // Generating combinations with actions and elements
 for (let i = 0; i < actions.length; i++) {
   for (let j = 0; j < elements.length; j++) {
     commands.push({
       command: `${actions[i]} ${elements[j]}`,
-      type: "simpleAction",
-    });
+      type: 'simpleAction'
+    })
   }
 }
 
 // Saving combinations generated above to the file commands.json
-fs.writeFileSync("./src/api/datasets/commands.json", JSON.stringify(commands));
+fs.writeFileSync('./src/api/datasets/commands.json', JSON.stringify(commands))
