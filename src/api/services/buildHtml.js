9929d253ac0c2255593960177ae0e0cc321f58fb
Olexander Mushtyn <57284217+olexandermushtyn@users.noreply.github.com>
2021-08-27 13:26:38 +0300

    add code edit component

--- src/api/services/buildHtml.js
+++ src/api/services/buildHtml.js
@@ -1,6 +1,6 @@
-const fs = require('fs')
-const { func } = require('joi')
-const json2html = require('node-json2html')
+const fs = require("fs");
+const { func } = require("joi");
+const json2html = require("node-json2html");
 
 // let buildedCommand = {
 //   action: 'add',
@@ -10,73 +10,73 @@ const json2html = require('node-json2html')
 
 function buildHtml(buildedCommand) {
   let template = JSON.parse(
-    fs.readFileSync('./src/api/jsonRes/modelOfHtml.json', 'utf-8')
-  )
+    fs.readFileSync("./src/api/jsonRes/modelOfHtml.json", "utf-8")
+  );
   let components = JSON.parse(
-    fs.readFileSync('./src/api/jsonRes/jsonComponents.json')
-  )
-
+    fs.readFileSync("./src/api/jsonRes/jsonComponents.json")
+  );
+  console.log(buildedCommand);
   //clean up template from null and undefinded
   template = template.filter(function (el) {
-    return el != null
-  })
+    return el != null;
+  });
 
-  if (buildedCommand.action == 'add') {
-    let component = components[buildedCommand.element]
+  if (buildedCommand.action == "add") {
+    let component = components[buildedCommand.element];
 
-    component['id'] = findFreeId(template)
-    template.push(component)
-  } else if (buildedCommand.action == 'delete') {
+    component["id"] = findFreeId(template);
+    template.push(component);
+  } else if (buildedCommand.action == "delete") {
     //find by id and delete this element
-    deleteById(template, buildedCommand.id)
+    deleteById(template, buildedCommand.id);
   }
 
-  let html = json2html.render({}, template)
+  let html = json2html.render({}, template);
 
   fs.writeFileSync(
-    './src/api/jsonRes/modelOfHtml.json',
+    "./src/api/jsonRes/modelOfHtml.json",
     JSON.stringify(template)
-  )
-  return html
+  );
+  return html;
   // fs.writeFileSync('./src/api/jsonRes/result.html', html)
 }
 
 function findFreeId(template) {
   //find all existed id
-  let existedId = []
-  existedId = findAllId(template, existedId)
-  let i = 1
+  let existedId = [];
+  existedId = findAllId(template, existedId);
+  let i = 1;
   while (true) {
     if (!existedId.includes(i)) {
-      return i.toString()
-    } else i++
+      return i.toString();
+    } else i++;
   }
 }
 
 function findAllId(template, existedId) {
   for (const key of Object.keys(template)) {
-    console.log(template[key])
-    if (isNumeric(template[key]['id']))
-      existedId.push(parseInt(template[key]['id']))
-    if (Array.isArray(template[key]['html'])) {
-      existedId = findAllId(template[key]['html'], existedId)
+    console.log(template[key]);
+    if (isNumeric(template[key]["id"]))
+      existedId.push(parseInt(template[key]["id"]));
+    if (Array.isArray(template[key]["html"])) {
+      existedId = findAllId(template[key]["html"], existedId);
     }
   }
-  return existedId
+  return existedId;
 }
 function deleteById(template, id) {
   for (const key of Object.keys(template)) {
-    if (template[key]['id'] == id) {
-      delete template[key]
-      return
-    } else if (Array.isArray(template[key]['html'])) {
-      deleteById(template[key]['html'], id)
+    if (template[key]["id"] == id) {
+      delete template[key];
+      return;
+    } else if (Array.isArray(template[key]["html"])) {
+      deleteById(template[key]["html"], id);
     }
   }
 }
 
 function isNumeric(value) {
-  return /^\d+$/.test(value)
+  return /^\d+$/.test(value);
 }
 
-module.exports = buildHtml
+module.exports = buildHtml;
