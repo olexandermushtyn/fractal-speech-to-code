8316a10f8d1bee80f51a6b49aae40dbbc1190d56
Olexander Mushtyn <57284217+olexandermushtyn@users.noreply.github.com>
2021-08-27 16:52:09 +0300

    add route for client

--- app.js
+++ app.js
@@ -1,61 +1,64 @@
-const express = require('express')
-const fs = require('fs')
-const logger = require('./src/global/logger')
-const routesV1 = require('./src/api/domains/routes')
-const cors = require('cors')
+const express = require("express");
+const fs = require("fs");
+const logger = require("./src/global/logger");
+const routesV1 = require("./src/api/domains/routes");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || '3000'
+const port = process.env.PORT || "3000";
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Content-Type', 'application/json;charset=UTF-8')
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Content-Type", "application/json;charset=UTF-8");
 
   // Request methods you wish to allow
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  )
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
   // Request headers you wish to allow
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type',
-    'Content-type'
-  )
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type",
+    "Content-type"
+  );
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   // res.setHeader("Access-Control-Allow-Credentials", true);
 
   // Pass to next layer of middleware
-  next()
-})
+  next();
+});
 
 app.use(
   express.urlencoded({
-    extended: false
+    extended: false,
   })
-)
+);
 
 // app.use(express.text())
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors())
-
+app.use(cors());
 
 // const router = express.Router()
 // router.get('/commands', function (req, res) {
 //   res.send('Birds home page')
 // })
-app.use('/api', routesV1)
+app.use("/api", routesV1);
 
-fs.writeFileSync('./src/api/jsonRes/modelOfHtml.json', "[]")
+app.use("/", (req, res) => {
+  res.sendFile("./src/client/build/index.html");
+});
+
+fs.writeFileSync("./src/api/jsonRes/modelOfHtml.json", "[]");
 
 app.listen(port, () => {
-  logger.info(`Listening at port: ${port}`)
-})
+  logger.info(`Listening at port: ${port}`);
+});
 
-module.exports = app
+module.exports = app;
